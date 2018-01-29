52279cd16ccabe98e40fb23cdccf57dbae64828d
Alexey V <alevor657@gmail.com>
2018-01-29 14:30:48 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace SocketServer
+{
+    public class Server : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine(e.Data.ToString());
+            var msg = e.Data.ToString().ToUpper();
+
+            Send(msg);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer("ws://localhost");
+            wssv.AddWebSocketService<Server>("/Server");
+            wssv.Start();
+            Console.WriteLine("Listening...");
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# This is a server to control the stuffa application thru the remote
